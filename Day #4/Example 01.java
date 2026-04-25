e2b763b792e38a49868f6cbcd714fd6a738f9c78
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-04-25 23:43:33 +0600

    Add Person and Teacher classes with display methods

--- Day #4/Example 01.java
+++ Day #4/Example 01.java
@@ -1,1 +1,44 @@
+package Inheritance;
+
+public class Person {
+    String Name;
+    int age;
+
+    void DisplayInformation1() {
+        System.out.println("Name : "+Name);
+        System.out.print("Age : "+age);
+    }
+}
+
+
+package Inheritance;
+
+public class Teacher extends Person {
+    //Name,age,DisplayInformation
+    String Qualification;
+    void DisplayInformation2() {
+        System.out.println("Name : "+Name);
+        System.out.println("Age : "+age);
+        System.out.println("Qualification : "+Qualification);
+    }
+}
+
+package Inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher();
+        t1.Name = "Md Aminul Islam";
+        t1.age = 55;
+        t1.Qualification = "Msc. in Physics";
+        t1.DisplayInformation2();
+
+        Teacher t2 = new Teacher();
+        t2.Name = "Dilip Chandra Karmaker";
+        t2.age = 60;
+        t2.Qualification = "Msc. in Math";
+        t2.DisplayInformation2();
+
+    }
+}
 
