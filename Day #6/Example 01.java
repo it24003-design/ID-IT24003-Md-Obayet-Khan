1bd59c1afbcf40b4c7d1d3586fe8619d3e90b904
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-04-26 09:22:09 +0600

    Update Example 01.java

--- Day #6/Example 01.java
+++ Day #6/Example 01.java
@@ -1,1 +1,38 @@
+package Abstruction;
+
+public abstract class MobileUser {
+    abstract void SendMassage();
+}
+
+package Abstruction;
+
+public class Rahim extends MobileUser {
+    void SendMassage() {
+        System.out.println("Hi!, I am Obayet");
+    }
+}
+
+package Abstruction;
+
+public class Karim extends MobileUser{
+    @Override
+    void SendMassage() {
+        System.out.println("hi! i am karim");
+
+    }
+}
+
+package Abstruction;
+
+public class Test {
+    public static void main(String[] args) {
+        MobileUser ms;
+
+        ms = new Karim();
+        ms.SendMassage();
+
+        ms = new Rahim();
+        ms.SendMassage();
+    }
+}
 
