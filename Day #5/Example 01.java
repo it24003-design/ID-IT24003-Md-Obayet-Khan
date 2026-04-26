9a86ea520c67f58f18da84617a09cc9111021129
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-04-26 07:51:38 +0600

    Implement polymorphism with Person, Teacher, and Student

--- Day #5/Example 01.java
+++ Day #5/Example 01.java
@@ -1,1 +1,39 @@
+package Polymorphisms;
+
+public class Person {
+    void Display() {
+        System.out.println("I am Obayet");
+    }
+}
+
+package Polymorphisms;
+
+public class Teacher extends Person {
+    @Override
+    void Display() {
+        System.out.println("I am a Teacher");
+    }
+}
+
+package Polymorphisms;
+
+public class Student extends Person {
+    void Display() {
+        System.out.println("I am a student");
+    }
+
+}
+
+package Polymorphisms;
+
+public class Test {
+    public static void main(String[] args) {
+        Person P = new Person();
+        P.Display();
+        P = new Teacher();
+        P.Display();
+        P = new Student();
+        P.Display();
+    }
+}
 
