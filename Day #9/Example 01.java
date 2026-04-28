34b77716af6ca7b17e64efb019900007f65ca58e
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-04-29 00:35:25 +0600

    Add Example 01.java with ArrayList demonstration

---
+++ Day #9/Example 01.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("C++");
+        list.add("Python");
+
+        System.out.println(list);
+        System.out.println(list.get(1));
+    }
+}
