32b9265a59586a0c40fa3b2a30fcf2bca895818f
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-04-29 00:30:50 +0600

    Update Example 02.java

--- Day #5/Example 02.java
+++ Day #5/Example 02.java
@@ -1,1 +1,17 @@
+class MathOperation {
+    int add(int a, int b) {
+        return a + b;
+    }
 
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperation obj = new MathOperation();
+        System.out.println(obj.add(5, 10));
+        System.out.println(obj.add(5, 10, 15));
+    }
+}
