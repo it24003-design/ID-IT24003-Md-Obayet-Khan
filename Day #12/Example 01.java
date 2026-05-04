ad206b006396e29fa5ceb95c80a44b2ae7f97706
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-05-04 09:33:03 +0600

    Update Example 01.java

--- Day #12/Example 01.java
+++ Day #12/Example 01.java
@@ -1,1 +1,16 @@
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("Apple", 50);
+        map.put("Banana", 30);
+        map.put("Mango", 70);
+
+        Access value        System.out.println("Price of Apple: " + map.get("Apple"));
+
+        System.out.println(map);
+    }
+}
