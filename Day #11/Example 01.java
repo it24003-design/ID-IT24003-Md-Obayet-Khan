4429a5b073f335d86f60cdf7e21aa690284df73e
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-05-04 09:34:03 +0600

    Update Example 01.java

--- Day #11/Example 01.java
+++ Day #11/Example 01.java
@@ -1,1 +1,20 @@
+import java.util.Queue;
+import java.util.LinkedList;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Queue<String> queue = new LinkedList<>();
+
+        queue.add("A");
+        queue.add("B");
+        queue.add("C");
+
+        System.out.println("Queue: " + queue);
+
+        queue.remove();
+        System.out.println("After remove: " + queue);
+
+        System.out.println("Front element: " + queue.peek());
+    }
+}
