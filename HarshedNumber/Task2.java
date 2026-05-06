4c3f7e3dd2cf2889e77cf2cc57a32af15ff3ea34
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-05-06 10:30:22 +0600

    Refactor HarshedNumber class into Solution class

--- HarshedNumber/Task2.java
+++ HarshedNumber/Task2.java
@@ -1,20 +1,16 @@
-import java.util.*;
-public class HarshedNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int n) {
         int x = 0;
         int n1 = n;
 
         while (n != 0) {
-            x = x + n % 10;
-            n = n / 10;
+            x += n % 10;
+            n /= 10;
         }
 
         if (n1 % x == 0)
-            System.out.println(x);
+            return x;
         else
-            System.out.println(-1);
+            return -1;
     }
 }
