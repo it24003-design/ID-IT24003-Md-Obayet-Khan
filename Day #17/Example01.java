2cdd6b2fb522154d3fe4db73b81c0f2fdd393a34
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-05-11 03:56:12 +0600

    Implement directory creation and deletion
    Create directories and attempt to delete one.

--- Day #17/Example01.java
+++ Day #17/Example01.java
@@ -1,1 +1,22 @@
+package File_Handling;
+
+import java.io.File;
+
+public class File01 {
+    public static void main(String[] args) {
+        File dir = new File("C:/Users/Lenovo/OneDrive/Desktop/2nd Year 2nd Semester/Person");
+        dir.mkdir();
+        File dir2 = new File("Person2");
+        dir2.mkdir();
+        String dirlocation=  dir.getAbsolutePath();
+        System.out.println(dirlocation);
+        System.out.println(dir.getName());
+
+        if(dir2.delete()) {
+            System.out.println(dir2.getName() + " Folder has been deleted!!!");
+        }
+
+    }
+
+}
 
