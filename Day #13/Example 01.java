dd49f07ad8bf74c446ac9a2d98fd1fde2a12bf08
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-05-10 08:28:57 +0600

    Update Example 01.java

--- Day #13/Example 01.java
+++ Day #13/Example 01.java
@@ -1,1 +1,36 @@
+package Array_List_Anisul;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Array_List1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        System.out.println("Size : "+number.size());
+
+        //Adding Elements
+        number.add(10);
+        number.add(20);
+        number.add(30);
+        number.add(3,50);
+
+        Iterator itr = number.iterator();
+        while(itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+        System.out.println();
+        for(int i : number) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Size : "+number.size());
+        System.out.println(number);
+
+        //Removing Elements
+        number.remove(2);
+        System.out.println("Size : "+number.size());
+        System.out.println(number);
+
+
+    }
+}
