dd6d0ee3fe9f1830c8ee56841a75e0ceea20b35c
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-05-13 00:41:28 +0600

    Update and rename Example 01.java to Example01.java

---
+++ Day #21/Example01.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "123";
+
+        int num = Integer.parseInt(s);
+
+        System.out.println(num);
+    }
+}
