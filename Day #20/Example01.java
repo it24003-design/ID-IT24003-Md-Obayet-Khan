04f5fc1a8b4dc0300bc941439145eeb2a9e745b8
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-05-13 00:40:20 +0600

    Display current time in Asia/Dhaka timezone
    Added functionality to display current time in Dhaka timezone.

--- Day #20/Example01.java
+++ Day #20/Example01.java
@@ -1,1 +1,13 @@
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
+public class Main {
+    public static void main(String[] args) {
+
+        ZoneId zone = ZoneId.of("Asia/Dhaka");
+
+        ZonedDateTime time = ZonedDateTime.now(zone);
+
+        System.out.println("Current Time : " + time);
+    }
+}
