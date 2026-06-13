1fad23babee5dd81669b4ff00c1c62c528db3383
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-06-13 23:32:22 +0600

    Create Main.java

---
+++ LabTest01/Main.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<UniversityMember> members = new ArrayList<>();
+
+        members.add(
+                new Professor(
+                        100006,
+                        "Dr. XYZ",
+                        130000,
+                        "CSE",
+                        "CyberSecurity",
+                        45
+                )
+        );
+
+        members.add(
+                new AdministrativeOfficer(
+                        50001,
+                        "Mr. Karim",
+                        60000,
+                        "Registrar Office"
+                )
+        );
+
+        System.out.println("===== Professor's Details =====");
+
+        for (UniversityMember member : members) {
+
+            if (member instanceof Professor) {
+
+                Professor p = (Professor) member;
+
+                p.displayBasicInfo();
+                p.showSalary();
+                p.showDepartment();
+                p.showResearchProfile();
+                p.performDuty();
+
+                System.out.println();
+            }
+        }
+
+        System.out.println("===== Administrative Officer Details =====");
+
+        for (UniversityMember member : members) {
+
+            if (member instanceof AdministrativeOfficer) {
+
+                AdministrativeOfficer ao =
+                        (AdministrativeOfficer) member;
+
+                ao.displayBasicInfo();
+                ao.showSalary();
+                ao.showOffice();
+                ao.performDuty();
+
+                System.out.println();
+            }
+        }
+    }
+}
