028f876d21123c47f321773b9c54eca661c4392c
Md Obayet Khan <it24003@mbstu.ac.bd>
2026-06-13 23:30:22 +0600

    Add Employee class extending UniversityMember

---
+++ LabTest01/Employee.java
@@ -0,0 +1,18 @@
+public class Employee extends UniversityMember {
+
+    protected double salary;
+
+    public Employee(int memberId, String name, double salary) {
+        super(memberId, name);
+        this.salary = salary;
+    }
+
+    public void showSalary() {
+        System.out.println("Salary: " + salary);
+    }
+
+    @Override
+    public void performDuty() {
+        System.out.println(name + " performs employee duties.");
+    }
+}
